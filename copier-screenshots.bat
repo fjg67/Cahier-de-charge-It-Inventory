8039e616b56b7be255f64a37462ebb77c42287fe
fjg67 <146087483+fjg67@users.noreply.github.com>
2026-02-13 00:30:55 +0100

    Ajout screenshots dans le dossier assets

---
+++ copier-screenshots.bat
@@ -0,0 +1,24 @@
+@echo off
+set SRC=C:\Users\flori\AppData\Roaming\Cursor\User\workspaceStorage\2e0352ff860089be4ada9f9e57222bcc\images
+set DST=c:\Users\flori\Documents\Projet\Cahier de charge StockPro\assets
+set PREFIX=c__Users_flori_AppData_Roaming_Cursor_User_workspaceStorage_2e0352ff860089be4ada9f9e57222bcc_images_
+
+echo Copie des screenshots...
+
+copy "%SRC%\image-26e2a576-aad7-47b4-9edc-1da2b7ff6302.png" "%DST%\%PREFIX%image-26e2a576-aad7-47b4-9edc-1da2b7ff6302.png"
+copy "%SRC%\image-292a031f-0f2b-4f27-8935-a7ba413922c6.png" "%DST%\%PREFIX%image-292a031f-0f2b-4f27-8935-a7ba413922c6.png"
+copy "%SRC%\image-07092974-0807-4a3c-a917-6f2e63b7d001.png" "%DST%\%PREFIX%image-07092974-0807-4a3c-a917-6f2e63b7d001.png"
+copy "%SRC%\image-34c8516d-a558-4bdc-ac76-1666de64a2de.png" "%DST%\%PREFIX%image-34c8516d-a558-4bdc-ac76-1666de64a2de.png"
+copy "%SRC%\image-354755b8-4e27-418c-bc18-0e328df1a707.png" "%DST%\%PREFIX%image-354755b8-4e27-418c-bc18-0e328df1a707.png"
+copy "%SRC%\image-70bc7db2-e262-4984-9d72-956cbdaf9d0b.png" "%DST%\%PREFIX%image-70bc7db2-e262-4984-9d72-956cbdaf9d0b.png"
+copy "%SRC%\image-d060235a-4328-4e79-94f2-9931ca734cde.png" "%DST%\%PREFIX%image-d060235a-4328-4e79-94f2-9931ca734cde.png"
+copy "%SRC%\image-0c89b1ce-cccc-4696-a63e-82160cae376e.png" "%DST%\%PREFIX%image-0c89b1ce-cccc-4696-a63e-82160cae376e.png"
+copy "%SRC%\image-f7d1ddef-2bc2-49c1-937b-bc21525b5067.png" "%DST%\%PREFIX%image-f7d1ddef-2bc2-49c1-937b-bc21525b5067.png"
+copy "%SRC%\image-3510ff32-b203-4a37-bb0b-c05c9c78cb5f.png" "%DST%\%PREFIX%image-3510ff32-b203-4a37-bb0b-c05c9c78cb5f.png"
+copy "%SRC%\image-510b4253-026b-4734-91f5-cc59b1fa5b93.png" "%DST%\%PREFIX%image-510b4253-026b-4734-91f5-cc59b1fa5b93.png"
+copy "%SRC%\image-cf6fa4a8-44c2-45d6-89fb-6cd012b9aabf.png" "%DST%\%PREFIX%image-cf6fa4a8-44c2-45d6-89fb-6cd012b9aabf.png"
+
+echo.
+echo Copie terminee !
+echo.
+pause
